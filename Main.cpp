9000c5cdc491d2a1aaf0c9ece39aac56b087476d
jaiCorpuz <jianna.corpuz@student.ateneo.edu>
2026-02-08 02:17:19 +0800

    implemented statemachines, no camera yet

--- Main.cpp
+++ Main.cpp
@@ -1,19 +1,37 @@
 #include <raylib.h>
-#include <raymath.h>
+#include "Player.h"
+
 
 const float WINDOW_WIDTH(800);
 const float WINDOW_HEIGHT(600);
 
-Vector2 position = {WINDOW_WIDTH/2, WINDOW_HEIGHT/2};
 
 int main() {
     SetConfigFlags(FLAG_WINDOW_HIGHDPI);
     InitWindow(WINDOW_WIDTH, WINDOW_HEIGHT, "State Machines");
 
+    SetTargetFPS(60.0f);
+    Vector2 position = {400, 300};
+    Player player ({400, 300});
+
+    // int cam_type = 0;
+    // Camera2D camera = {0};
+    // camera.target = position;
+    // camera.offset = {WINDOW_WIDTH/2, WINDOW_HEIGHT/2};
+    // camera.zoom = 1.0f;
+
     while (!WindowShouldClose()) {
+        float delta_time = GetFrameTime();
+
+        player.Update(delta_time);
+
+        // camera.target = position;
+
         BeginDrawing();
+        //BeginMode2D(camera);
         ClearBackground(DARKGRAY);
-        DrawCircleV(position, 40.0f, MAROON);
+        player.Draw();
+        //EndMode2D();
         EndDrawing();
     }
     CloseWindow();
