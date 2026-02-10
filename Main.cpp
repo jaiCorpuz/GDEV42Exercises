d9e28eb92bf58a3a37b417d1283dd22804315957
jaiCorpuz <jianna.corpuz@student.ateneo.edu>
2026-02-10 11:20:53 +0800

    implemented camera with clamping

--- Main.cpp
+++ Main.cpp
@@ -4,10 +4,31 @@
 const float WINDOW_WIDTH(800);
 const float WINDOW_HEIGHT(600);
 
+// Vector2 MIN_EDGE = {-300.0f, -500.0f};
 Vector2 MIN_EDGE = {-500.0f, -300.0f};
-Vector2 MAX_EDGE = {538.0f, 1620.0f};
+// Vector2 MAX_EDGE = {538.0f, 1620.0f};
+Vector2 MAX_EDGE = {1420.0f, 738.0f};
 
+void ClampPlayer(Player& player, Vector2 min_edge, Vector2 max_edge, float radius) {
+    Vector2 player_position = player.GetPosition();
+    
+    player_position.x = (player_position.x - radius < min_edge.x) ? min_edge.x + radius : (player_position.x + radius > max_edge.x ? max_edge.x - radius : player_position.x);
+    player_position.y = (player_position.y - radius < min_edge.y) ? min_edge.y + radius : (player_position.y + radius > max_edge.y ? max_edge.y - radius : player_position.y);
 
+    player.SetPosition(player_position);
+
+}
+
+void ClampCamera(Player& player, Vector2 min_edge, Vector2 max_edge, Camera2D& camera) {
+    Vector2 half_view = {(GetScreenWidth()/2)/camera.zoom, (GetScreenHeight()/2)/camera.zoom};
+
+    float cam_min_x = min_edge.x + half_view.x;
+    float cam_min_y = min_edge.y + half_view.y;
+    float cam_max_x = max_edge.x - half_view.x;
+    float cam_max_y = max_edge.y - half_view.y;
+
+    camera.target = Vector2Clamp(camera.target, {cam_min_x, cam_min_y}, {cam_max_x, cam_max_y});
+}
 
 int main() {
     SetConfigFlags(FLAG_WINDOW_HIGHDPI);
@@ -29,8 +50,14 @@ int main() {
         float delta_time = GetFrameTime();
 
         player.Update(delta_time);
+        
+        ClampPlayer(player, MIN_EDGE, MAX_EDGE, 40.0f);
 
         camera.target = player.GetPosition();
+
+        ClampCamera(player, MIN_EDGE, MAX_EDGE, camera);
+
+
 
         BeginDrawing();
         BeginMode2D(camera);
